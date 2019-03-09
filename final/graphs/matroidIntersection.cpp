523fdcd0ad35a6aea3b2843d8cba05b787c26625
roman <romanfml31@gmail.com>
2019-03-09 12:08:55 +0300

    add suffArrayLinear and Manacher

--- final/graphs/matroidIntersection.cpp
+++ final/graphs/matroidIntersection.cpp
@@ -1,74 +1,81 @@
-    // check(ctaken, 1) -- first matroid
-    // check(ctaken, 2) -- second matroid    
-    vector<char> taken(m);
-    while (1) {
-        vector<vector<int>> e(m);
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < m; j++) {
-                if (taken[i] && !taken[j]) {
-                    auto ctaken = taken;
-                    ctaken[i] = 0;
-                    ctaken[j] = 1;
-                    if (check(ctaken, 2)) {
-                        e[i].push_back(j);
-                    }
-                }
-                if (!taken[i] && taken[j]) {
-                    auto ctaken = taken;
-                    ctaken[i] = 1;
-                    ctaken[j] = 0;
-                    if (check(ctaken, 1)) {
-                        e[i].push_back(j);
-                    }
-                }
-            }
-        }
-        vector<int> type(m);
-        // 0 -- cant, 1 -- can in \2, 2 -- can in \1
-        for (int i = 0; i < m; i++) {
-            if (!taken[i]) {
-                auto ctaken = taken;
-                ctaken[i] = 1;
-                if (check(ctaken, 2)) type[i] |= 1;
-            }
-            if (!taken[i]) {
-                auto ctaken = taken;
-                ctaken[i] = 1;
-                if (check(ctaken, 1)) type[i] |= 2;
-            }
-        }
-        vector<int> w(m);
-        for (int i = 0; i < m; i++) {
-            w[i] = taken[i] ? ed[i].c : -ed[i].c;
-        }
-        vector<pair<int, int>> d(m, {INF, 0});
-        for (int i = 0; i < m; i++) {
-            if (type[i] & 1) d[i] = {w[i], 0};
-        }
-        vector<int> pr(m, -1);
-        while (1) {
-            vector<pair<int, int>> nd = d;
-            for (int i = 0; i < m; i++) {
-                if (d[i].first == INF) continue;
-                for (int to : e[i]) {
-                    if (nd[to] > make_pair(d[i].first + w[to], d[i].second + 1)) {
-                        nd[to] = make_pair(d[i].first + w[to], d[i].second + 1);
-                        pr[to] = i;
-                    }
-                }
-            }
-            if (d == nd) break;
-            d = nd;
-        }
-        int v = -1;
-        for (int i = 0; i < m; i++) {
-            if ((d[i].first < INF && (type[i] & 2)) && (v == -1 || d[i] < d[v])) v = i;
-        }
-        if (v == -1) break;
-        while (v != -1) {
-            sum += w[v];
-            taken[v] ^= 1;
-            v = pr[v];
-        }
-        ans[--cnt] = sum;
+struct Graph {
+    vector<vector<int>> G;
+
+    Graph(int n = 0) {
+        G.resize(n);
     }
+
+    void add_edge(int v, int u) {
+        G[v].push_back(u);
+    }
+
+    vector<int> get_path(vector<int> &s, vector<int> &t) {
+        int n = G.size();
+        vector<int> dist(n, inf), pr(n, -1);
+        queue<int> Q;
+        for (int i : s) {
+            dist[i] = 0;
+            Q.push(i);
+        }
+        while (!Q.empty()) {
+            int v = Q.front();
+            Q.pop();
+            for (int to : G[v]) if (dist[to] > dist[v] + 1) {
+                dist[to] = dist[v] + 1;
+                pr[to] = v;
+                Q.push(to);
+            }
+        }
+        int V = -1;
+        for (int i : t) if (V == -1 || dist[i] < dist[V]) {
+            V = i;
+        }
+        if (V == -1 || dist[V] == inf) return {};
+        vector<int> path;
+        while (V != -1) {
+            path.push_back(V);
+            V = pr[V];
+        }
+        return path;
+    }
+};
+
+void get_ans(vector<int> &used, int m) {
+    Graph G(m);
+    for (int i = 0; i < m; ++i) if (used[i]) {
+        Gauss gauss;
+        vector<int> color(130, 0);
+        for (int j = 0; j < m; ++j) if (used[j] && j != i) {
+                gauss.add(a[j]);
+                color[c[j]] = 1;
+            }
+        for (int j = 0; j < m; ++j) if (!used[j]) {
+            if (gauss.check(a[j])) {
+                G.add_edge(i, j);
+            }
+            if (!color[c[j]]) {
+                G.add_edge(j, i);
+            }
+        }
+    }
+
+    Gauss gauss;
+    vector<int> color(130, 0);
+    for (int i = 0; i < m; ++i) if (used[i]) {
+        gauss.add(a[i]);
+        color[c[i]] = 1;
+    }
+    vector<int> x1, x2;
+    for (int i = 0; i < m; ++i) if (!used[i]) {
+        if (gauss.check(a[i])) {
+            x1.push_back(i);
+        }
+        if (!color[c[i]]) {
+            x2.push_back(i);
+        }
+    }
+    vector<int> path = G.get_path(x1, x2);
+    if (!path.size()) return;
+    for (int i : path) used[i] ^= 1;
+    get_ans(used, m);
+}
